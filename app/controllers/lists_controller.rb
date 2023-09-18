e1858af33ade3c39ced2b7b84effd96de0fe09f1
Soojin Kim <contact.soojin.kim@gmail.com>
2023-09-18 16:27:13 +0200

    finished bookmark part of challenge 2

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,45 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: %i[ show edit update destroy ]
+
+  # GET /lists
+  def index
+    @lists = List.all
+  end
+
+  # GET /lists/1
+  def show
+  end
+
+  # GET /lists/new
+  def new
+    @list = List.new
+  end
+
+  # POST "lists"
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to @list, notice: "A list has been successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # POST "lists"
+  def destroy
+    @list.destroy
+    redirect_to lists_url, notice: "The list has been successfully destroyed.", status: :see_other
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :movies
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
 end
